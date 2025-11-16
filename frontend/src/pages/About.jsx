5fdb79009cbbb1ba9348249bebf53ba558f9fdc2
OmvedNagre <code64816@gmail.com>
2025-11-16 19:30:48 +0530

    Basically Front-End part is finsihed for now

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
+import NewsletterBox from '../components/NewsletterBox'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div className='text-2xl text-center pt-8 border-t border-gray-300'>
+        <Title text1={'ABOUT'} text2={'US'} />
+      </div>
+
+      <div className='my-10 flex flex-col md:flex-row gap-16'>
+        <img className='w-full md:max-w-[450px]' src={assets.about_img} alt="" />
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
+        <p>Staitc Dummy-1</p>
+        <p>Static Dummy-2</p>
+        <b className='text-gray-800'>Our Mission</b>
+        <p>Our mission at ShopWise is </p>
+        </div>
+      </div>
+      
+      <div className='text-xl py-4'>
+        <Title text1={'WHY'} text2={'CHOOSE US'} />
+      </div>
+
+      <div className='flex flex-col md:flex-row text-sm mb-20 gap-5'>
+        <div className='border border-gray-200 px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5 md:flex-1'>
+          <b>Quality Assurance:</b>
+          <p className='text-gray-600' >All products go through strict quality checks before dispatch. We offer secure payment processing and simple return options for complete peace of mind. Experience safe, reliable, and worry-free shopping with us.</p>
+        </div>
+        <div className='border border-gray-200 px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5 md:flex-1'>
+          <b>Convenience:</b>
+          <p className='text-gray-600' >Shop comfortably with a platform designed for maximum convenience. From intuitive navigation and quick product discovery to fast checkout and flexible delivery options — everything is built to make your experience easier and stress-free.</p>
+        </div>
+        <div className='border border-gray-200 px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5 md:flex-1'>
+          <b>Exceptional Customer Service:</b>
+          <p className='text-gray-600' >Experience exceptional customer service with our committed support team. Whether you have questions, need assistance, or require personalized guidance, we’re here to help with quick responses, friendly communication, and a customer-first approach. Your satisfaction is our priority.</p>
+        </div>
+      </div>
+      <NewsletterBox/>
+
+    </div>
   )
 }
 
--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -25,7 +25,7 @@ const Cart = () => {
   },[cartItems])
 
   return (
-    <div className='border-t pt-14'>
+    <div className='border-t border-gray-300 pt-14'>
 
       <div className='text-2xl mb-3'>
         <Title text1={'YOUR'} text2={'CART'} />
@@ -40,7 +40,7 @@ const Cart = () => {
             if (!productData) return null;
 
             return (
-              <div key={index} className='py-4 border-t border-b border-gray-200 text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4'>
+              <div key={index} className='py-4 border-t border-b border-gray-00 text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4'>
                 <div className='flex items-start gap-6'>
                   <img className='w-16 sm:w-20' src={productData.image[0]} alt="" />
                   <div>
